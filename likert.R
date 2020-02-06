60d333d3ab60ea50d053cbb1f4d1c4d57b345faa
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-02-06 13:09:32 -0600

    untabify

--- likert.R
+++ likert.R
@@ -26,21 +26,21 @@ less_spiky    = likert_beta(samp_size, 1,   0.5)
 samp_names = c("centered", "r_tail", "l_tail", "concave", "more_concave", "ramp", "gentle_ramp", "ramp2", "moderate_ramp", "very_spiky", "less_spiky", "moderate_ramp")
 
 X = data.frame(score = centered, pop_name = rep("centered", length(centered)), 
-								group = rep("A", length(centered)),
-								question_num = rep(1, length(centered)))
+                                 group = rep("A", length(centered)),
+                                 question_num = rep(1, length(centered)))
 npanel = 1
 for (s in samp_names[-1]){
-	tp = "A"
-	if(npanel %% 2 == 1){
-		tp = "B"
-	}
-	qn = floor(npanel / 2) + 1
-	temp_vec = eval(parse(text=s))
-	temp_frame = data.frame(score = temp_vec, pop_name = rep(s, length(temp_vec)), 
-											group = rep(tp, length(temp_vec)),
-											question_num = rep(qn, length(temp_vec)))
-	X = rbind(X, temp_frame)
-	npanel = npanel + 1
+        tp = "A"
+        if(npanel %% 2 == 1){
+                tp = "B"
+        }
+        qn = floor(npanel / 2) + 1
+        temp_vec = eval(parse(text=s))
+        temp_frame = data.frame(score = temp_vec, pop_name = rep(s, length(temp_vec)), 
+                                                  group = rep(tp, length(temp_vec)),
+                                                  question_num = rep(qn, length(temp_vec)))
+        X = rbind(X, temp_frame)
+        npanel = npanel + 1
 }
 
 
@@ -62,19 +62,19 @@ g + geom_bar(aes(fill=as.factor(score))) # kind of like spineplot, colors not gr
 Results = data.frame()
 
 for (q in 1:6){
-	wt = wilcox.test(X[X$question_num == q & X$group == "A", ]$score,
-				X[X$question_num == q & X$group == "B", ]$score)
-	tt = t.test(score ~ group, data = X[X$question_num == q, ])
-	tab = with(X[ X$question_num == q, ], table(score, group)) # 5x2 table
-	catt = prop.trend.test(tab[,1], apply(tab,1, sum))
-	# https://www.rdocumentation.org/packages/DescTools/versions/0.99.32/topics/CochranArmitageTest
+        wt = wilcox.test(X[X$question_num == q & X$group == "A", ]$score,
+                         X[X$question_num == q & X$group == "B", ]$score)
+        tt = t.test(score ~ group, data = X[X$question_num == q, ])
+        tab = with(X[ X$question_num == q, ], table(score, group)) # 5x2 table
+        catt = prop.trend.test(tab[,1], apply(tab,1, sum))
+        # https://www.rdocumentation.org/packages/DescTools/versions/0.99.32/topics/CochranArmitageTest
 
-	temp_frame = data.frame(question = q, wilcoxon_p = wt$p.value, ttest_p = tt$p.value, catt_p = catt$p.value)
-	Results= rbind(Results, temp_frame)
-	#print(q)
-	#print(wt)
-	#print(tt)
-	#print(catt)
+        temp_frame = data.frame(question = q, wilcoxon_p = wt$p.value, ttest_p = tt$p.value, catt_p = catt$p.value)
+        Results= rbind(Results, temp_frame)
+        #print(q)
+        #print(wt)
+        #print(tt)
+        #print(catt)
 }
 
 tapply(X$pop_name, X[,-2], length) # 5x2 tables for each of 6 questions
