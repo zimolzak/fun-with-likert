483d04dc1d295717fe857443a18e8e8345897483
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-02-06 09:40:23 -0600

    Two skewed betas.

--- likert.R
+++ likert.R
@@ -1,6 +1,12 @@
 library(ggplot2)
 samp_size = 650
 unit_interval = c(0,1,2,3,4,5,6,7,8,9,10) / 10
-b22_sample = rbeta(samp_size, 2, 2)
-qplot(b22_sample)
 
+b22 = rbeta(samp_size, 2, 2)
+qplot(b22)
+
+b24 = rbeta(samp_size, 2, 4)
+qplot(b24)
+
+b42 = rbeta(samp_size, 4, 2)
+qplot(b42)
