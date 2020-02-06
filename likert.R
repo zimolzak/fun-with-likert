9474e1cb9ce1f87a0250d7a0c489fde9972a1859
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-02-06 10:03:41 -0600

    More named distribution shapes

--- likert.R
+++ likert.R
@@ -11,11 +11,11 @@ l_tail = likert_beta(samp_size, 4, 2)
 
 
 
-qplot(rbeta(samp_size, 8,   1))   # concave
-qplot(rbeta(samp_size, 8,   0.9)) # more concave?
-qplot(rbeta(samp_size, 2,   0.9)) # ramp
-qplot(rbeta(samp_size, 1.1, 0.9)) # gentle ramp
-qplot(rbeta(samp_size, 2,   1))   # ramp
-qplot(rbeta(samp_size, 1.5, 1))   # moderate ramp
-qplot(rbeta(samp_size, 1.5, 0.5)) # very spiky
-qplot(rbeta(samp_size, 1,   0.5)) # less spiky
+concave = likert_beta(samp_size, 8,   1)
+more_concave = likert_beta(samp_size, 8,   0.9)
+ramp = likert_beta(samp_size, 2,   0.9)
+gentle_ramp = likert_beta(samp_size, 1.1, 0.9)
+ramp2 = likert_beta(samp_size, 2,   1)
+moderate_ramp = likert_beta(samp_size, 1.5, 1)
+very_spiky = likert_beta(samp_size, 1.5, 0.5)
+less_spiky = likert_beta(samp_size, 1,   0.5)
