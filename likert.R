f87cbbe70e60ff2e91af9faaa7652da715c2cd2f
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-02-07 13:47:21 -0600

    Slight refactoring for readability.

--- likert.R
+++ likert.R
@@ -2,44 +2,56 @@ library(ggplot2)
 
 #### GENERATE ASSORTED SAMPLES OF FAKE LIKERT DATA
 
-samp_size = 60
-max_likert = 4
+SAMP_SIZE = 60
+MAX_LIKERT = 4   # 0 to 4 Likert scale
 
 likert_beta = function(n, a, b){
-        round(rbeta(n, a, b) * max_likert)   # 0 to 4 Likert scale
+        round(rbeta(n, a, b) * MAX_LIKERT)
 }
 
-centered      = likert_beta(samp_size, 2,   2)
-r_tail        = likert_beta(samp_size, 2,   4)
-l_tail        = likert_beta(samp_size, 4,   2)
-concave       = likert_beta(samp_size, 8,   1)
-more_concave  = likert_beta(samp_size, 8,   0.9)
-ramp          = likert_beta(samp_size, 2,   0.9)
-gentle_ramp   = likert_beta(samp_size, 1.1, 0.9)
-ramp2         = likert_beta(samp_size, 2,   1)
-moderate_ramp = likert_beta(samp_size, 1.5, 1)
-very_spiky    = likert_beta(samp_size, 1.5, 0.5)
-less_spiky    = likert_beta(samp_size, 1,   0.5)
+centered      = likert_beta(SAMP_SIZE, 2,   2)
+r_tail        = likert_beta(SAMP_SIZE, 2,   4)
+l_tail        = likert_beta(SAMP_SIZE, 4,   2)
+concave       = likert_beta(SAMP_SIZE, 8,   1)
+more_concave  = likert_beta(SAMP_SIZE, 8,   0.9)
+ramp          = likert_beta(SAMP_SIZE, 2,   0.9)
+gentle_ramp   = likert_beta(SAMP_SIZE, 1.1, 0.9)
+ramp2         = likert_beta(SAMP_SIZE, 2,   1)
+moderate_ramp = likert_beta(SAMP_SIZE, 1.5, 1)
+very_spiky    = likert_beta(SAMP_SIZE, 1.5, 0.5)
+less_spiky    = likert_beta(SAMP_SIZE, 1,   0.5)
 
 
 #### BUILD UP A DATA FRAME
 
-samp_names = c("centered", "r_tail", "l_tail", "concave", "more_concave", "ramp", "gentle_ramp", "ramp2", "moderate_ramp", "very_spiky", "less_spiky", "moderate_ramp")
+# Group:        A                B
+
+samp_names = c("centered",      "r_tail",         # question 1
+	       "l_tail",        "concave",        # question 2
+	       "more_concave",  "ramp",		  # 3
+	       "gentle_ramp",   "ramp2",	  # 4
+	       "moderate_ramp", "very_spiky",	  # 5
+	       "less_spiky",    "moderate_ramp")  # 6
+
+# Q1 group A
 
 X = data.frame(score = centered, pop_name = rep("centered", length(centered)), 
                                  group = rep("A", length(centered)),
                                  question_num = rep(1, length(centered)))
+
+# Q1 group B, through Q6 group B
+
 npanel = 1
 for (s in samp_names[-1]){
-        tp = "A"
+        g = "A"
         if(npanel %% 2 == 1){
-                tp = "B"
+                g = "B"
         }
-        qn = floor(npanel / 2) + 1
+        q = floor(npanel / 2) + 1
         temp_vec = eval(parse(text=s))
         temp_frame = data.frame(score = temp_vec, pop_name = rep(s, length(temp_vec)), 
-                                                  group = rep(tp, length(temp_vec)),
-                                                  question_num = rep(qn, length(temp_vec)))
+                                                  group = rep(g, length(temp_vec)),
+                                                  question_num = rep(q, length(temp_vec)))
         X = rbind(X, temp_frame)
         npanel = npanel + 1
 }
@@ -50,17 +62,19 @@ for (s in samp_names[-1]){
 
 q7a = c(1, 3.2, 2, 1.5, 1)
 q7b = c(1, 1.5, 2, 3, 1)
-q7a = round(samp_size / sum(q7a) * q7a) # normalize
-q7b = round(samp_size / sum(q7b) * q7b)
+q7a = round(SAMP_SIZE / sum(q7a) * q7a) # normalize
+q7b = round(SAMP_SIZE / sum(q7b) * q7b)
 
-for (s in 0:max_likert){
+for (s in 0:MAX_LIKERT){
 	replicates_a = q7a[s+1]
 	replicates_b = q7b[s+1]
 	for (i in 1:replicates_a){
-		X = rbind(X, data.frame(score = s, pop_name = "sketched", group = "A", question_num = 7))
+		X = rbind(X, data.frame(score = s, pop_name = "sketched",
+		                        group = "A", question_num = 7))
 	}
 	for (i in 1:replicates_b){
-		X = rbind(X, data.frame(score = s, pop_name = "sketched", group = "B", question_num = 7))
+		X = rbind(X, data.frame(score = s, pop_name = "sketched",
+		                        group = "B", question_num = 7))
 	}
 }
 
@@ -68,7 +82,7 @@ for (s in 0:max_likert){
 #### PLOTS AND ANALYSIS
 
 p = ggplot(X, aes(score)) + geom_bar() + xlab("Likert score")
-p + facet_grid(rows = vars(question_num), cols = vars(group)) # best one yet
+p + facet_grid(rows = vars(question_num), cols = vars(group))
 ggsave("facet_grid.png")
 
 Results = data.frame()
@@ -82,7 +96,8 @@ for (q in 1:7){
         cs = prop.test(tab[,1], apply(tab,1, sum))
         # https://www.rdocumentation.org/packages/DescTools/versions/0.99.32/topics/CochranArmitageTest
 
-        temp_frame = data.frame(question = q, wilcoxon_p = wt$p.value, ttest_p = tt$p.value, catt_p = catt$p.value, chi_p = cs$p.value)
+        temp_frame = data.frame(question = q, wilcoxon_p = wt$p.value,
+	                        ttest_p = tt$p.value, catt_p = catt$p.value, chi_p = cs$p.value)
         Results= rbind(Results, temp_frame)
 }
 
